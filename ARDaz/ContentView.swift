19d250a7d984296c60023ba9570229f38d46450c
Jeffery <li.jianfei@alibaba-inc.com>
2023-12-18 12:11:49 +0800

    feature: 完成扫码需求

--- ARDaz/ContentView.swift
+++ ARDaz/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
-
+import CodeScanner
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            QRCodeScannerExampleView()
         }
         .padding()
+    }
+}
+
+struct QRCodeScannerExampleView: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String?
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            
+            Button("Scan Code") {
+                isPresentingScanner = true
+            }
+            
+            Text("Scan a QR code to begin")
+        }
+        .sheet(isPresented: $isPresentingScanner) {
+            CodeScannerView(codeTypes: [.qr]) { response in
+                switch response {
+                case .success(let result):
+                    scannedCode = result.string
+                    print("Found code: \(result.string)")
+                    isPresentingScanner = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    isPresentingScanner = false
+                }
+            }
+        }
     }
 }
 
