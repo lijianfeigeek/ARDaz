c3883ff8c9d0c9dc4f1e0da812d131cc26778eca
Jeffery <li.jianfei@alibaba-inc.com>
2023-12-18 12:40:29 +0800

    feature: 增加toast

--- ARDaz/ContentView.swift
+++ ARDaz/ContentView.swift
@@ -7,10 +7,20 @@
 
 import SwiftUI
 import CodeScanner
+import AlertToast
+
 struct ContentView: View {
+    @State  var showErrorToast = false
+    @State  var showSuccessToast = false
     var body: some View {
         VStack {
-            QRCodeScannerExampleView()
+            QRCodeScannerExampleView(showErrorToast: $showErrorToast,showSuccessToast:$showSuccessToast)
+            .toast(isPresenting: $showSuccessToast) {
+                AlertToast(type: .regular, title: "ARDaz will coming...")
+            }
+            .toast(isPresenting: $showErrorToast) {
+                AlertToast(type: .error(.red), title: "QR code error")
+            }
         }
         .padding()
     }
@@ -18,16 +28,20 @@ struct ContentView: View {
 
 struct QRCodeScannerExampleView: View {
     @State private var isPresentingScanner = false
+    
+    @Binding var showErrorToast: Bool
+    @Binding var showSuccessToast: Bool
+
     @State private var scannedCode: String?
     
     var body: some View {
         VStack(spacing: 10) {
             
-            Button("Scan Code") {
+            Button("Scan DazAR QR Code") {
                 isPresentingScanner = true
             }
             
-            Text("Scan a QR code to begin")
+            Text("Scan a DazAR QR code to begin")
         }
         .sheet(isPresented: $isPresentingScanner) {
             CodeScannerView(codeTypes: [.qr]) { response in
@@ -35,10 +49,14 @@ struct QRCodeScannerExampleView: View {
                 case .success(let result):
                     scannedCode = result.string
                     print("Found code: \(result.string)")
+                    // 下载模型
                     isPresentingScanner = false
+                    showSuccessToast.toggle()
+
                 case .failure(let error):
                     print(error.localizedDescription)
                     isPresentingScanner = false
+                    showErrorToast.toggle()
                 }
             }
         }
