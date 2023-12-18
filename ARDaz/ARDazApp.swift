9b707ee2d84f97bd20a7b657d654954ded59dc1f
Jeffery <li.jianfei@alibaba-inc.com>
2023-12-18 11:45:46 +0800

    Initial commit

---
+++ ARDaz/ARDazApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDazApp.swift
+//  ARDaz
+//
+//  Created by Jeffery on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ARDazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
